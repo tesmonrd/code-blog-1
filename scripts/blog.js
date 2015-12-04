7ad7477d53cc3210d4c52e52c9c238a5ad4f9981
natepecota <nate.pecota@gmail.com>
2015-12-04 08:36:28 -0800

    Changed css file from pixels to em and reordered blog.makeTabsWork variables.

--- scripts/blog.js
+++ scripts/blog.js
@@ -57,11 +57,11 @@ blog.makeTabsWork = function() {
     event.preventDefault();
     var active_tab_selector = $('.nav-tabs > li > a').attr('href');
     var active_nav = $('.nav-tabs > li.active');
+    var target_tab_selector = $(this).attr('href');
     active_nav.removeClass('active');
     $(this).parent('li').addClass('active');
     $(active_tab_selector).removeClass('active');
     $(active_tab_selector).addClass('hide');
-    var target_tab_selector = $(this).attr('href');
     $(target_tab_selector).removeClass('hide');
     $(target_tab_selector).addClass('active');
   });
