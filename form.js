6c530d14ff2d7f4b518cbff5dfb7436eedf38158
Rick Tesmond <tesmonrd@gmail.com>
2015-12-04 17:09:19 -0800

    Added handlebars, consturctor function, and everything to make preview render

--- form.js
+++ form.js
@@ -1,14 +1,36 @@
+var articleFromPage = [];
+
+var Article = function() {
+  this.title = $('#title').val();
+  this.category = $('#category').val();
+  this.author = $('#author').val();
+  this.authorUrl = $('#authorUrl').val();
+  this.publishedOn = $('#publishedOn').val();
+  this.body = $('#body').val();
+  articleFromPage.push(this);
+
+};
+
+Article.prototype.toHTML = function() {
+  var source = $('#article-template').html();
+  var template = Handlebars.compile(source);
+  var html = template(this);
+  return html;
+};
+
+Article.prototype.publish = function() {
+  var $template = this.toHTML();
+  $('#sampleArticle').append($template);
+};
+
+var buildComment = function () {
+  var blogPost = new Article (articleFromPage);
+  blogPost.publish();
+};
+
 $(document).ready(function() {
-  $('#newArticle').on('submit', function(event){
+  $('#Article').on('submit', function(event){
     event.preventDefault();
-    var newArticle = {
-      title: $('#title').val(),
-      category: $('#category').val(),
-      author: $('#author').val(),
-      authorUrl: $('#authorUrl').val(),
-      publishedOn: $('#publishedOn').val(),
-      body: $('body').val(),
-    };
-    console.log(newArticle);
+    buildComment();
   });
-})
+});
